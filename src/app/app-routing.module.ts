511d86031c2348ab58fa59b4c2cdcab2949795ec
Ajay <ajaythod159@gmail.com>
2020-08-07 12:55:54 +0530

    stackBlitz

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { WelcomeComponent } from './home/welcome.component';
+import { ProductListComponent } from './products/product-list.component';
 
+import { ProductDetailComponent } from './products/product-detail.component';
+import { ProductDetailGuard } from './products/product-detail.guard';
+// import { SharedModule } from '../shared/shared.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  
+  { path: 'welcome', component: WelcomeComponent },
+  // {path:'**',redirectTo:'welcome'}
+  
+  { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+  // { path: '**', redirectTo: 'welcome', pathMatch: 'full' },  
+  { path: 'products', component: ProductListComponent },
+  { path: 'products/:id',
+  canActivate: [ProductDetailGuard],
+   component: ProductDetailComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
